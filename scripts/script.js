9918df74a5ac27e1b16120e3c53844ba23032e89
Yasmina Maddah <maddahyasmina@gmail.com>
2024-11-03 17:58:10 +0200

    Tried finding the error

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,27 @@
 async function fetchWonders() {
-    try {
+  try {
       const response = await fetch('https://www.world-wonders-api.org/v0/wonders');
       const wonders = await response.json();
       displayWonders(wonders);
-    } catch (error) {
+  } catch (error) {
       console.error('Error fetching wonders:', error);
-    }
   }
+}
 
-  fetchWonders();
+fetchWonders();
 
-  function displayWonders(wonders) {
-    const wondersList = document.getElementById('wonders-list');
-    wondersList.innerHTML = wonders.map(wonder => `
-      <div class="wonder">
+function displayWonders(wonders) {
+  const wondersList = document.getElementById('wonders-list');
+  wondersList.innerHTML = wonders.map(wonder => 
+    `<div class="wonder">
         <h2>${wonder.name}</h2>
         <p>${wonder.location}</p>
         <button onclick="viewWonderDetails('${wonder.id}')">View Details</button>
-      </div>
-    `).join('');
-  }
+     </div>`
+  ).join('');
+}
 
-  function viewWonderDetails(id) {
-    window.location.href = `details.html?id=${id}`;
-  }
+function viewWonderDetails(id) {
+  console.log("Wonder ID:", id); 
+  window.location.href = `details.html?id=${id}`;
+}
